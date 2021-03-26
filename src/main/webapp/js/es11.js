def5cdcc10f6197d6904e4bbc977e0038da8f792
geeny <geeny03@naver.com>
2021-03-26 14:06:15 +0900

    es11 API - optinal chaining, Nullish coalessing operation

--- src/main/webapp/js/es11.js
+++ src/main/webapp/js/es11.js
@@ -4,7 +4,7 @@ function log(msg){
 } 
 console.clear();
 
-log('Optional Chaining (ES11)');
+log('## Optional Chaining (ES11)');
 const person1 = {
     name :'sally',
     job: {
@@ -24,7 +24,7 @@ function printManager2(person) {
 printManager(person1);
 printManager2(person2);
 
-log('Nullish Coalescing Operateor(ES11)');
+log('## Nullish Coalescing Operateor(ES11)');
 
 const name = '';
 const userName = name ?? 'Guest';
@@ -35,6 +35,41 @@ const message = num ?? 'undefiend';
 log(message);
 
 
+log('## BigInt (A BIGINT is always 8 bytes and can store -9223372036854775808 to 9223372036854775807)');
+const x = Number.MAX_SAFE_INTEGER + 1;
+const y = Number.MAX_SAFE_INTEGER + 2;
+log(x === y); //true
+
+//node 8 is not a supported runtime.
+const x1 = BigInt(9007199254740991);//9천조..
+const y1 = BigInt(9007199254740994);
+log(x1 === y1); //false 
+
+const x2  = 9007199254740991n;
+log( typeof x2);//bigint
+
+const obj = { a: 1, b:2, c:3};
+for(const key in obj ){
+    log(obj[key]);// 1 2 3
+}
+
+let str = 'I love JavsScript yes I do JavaScript';
+let result = str.matchAll(/JavaScript/g);//
+log(...result);
+/*
+[
+  'JavaScript',
+  index: 27,   
+  input: 'I love JavsScript yes I do JavaScript',
+  groups: undefined
+]
+*/
+
+const promise3 = Promise.resolve("another resolve");
+const promise1 = Promise.resolve("reject");
+const promise2 = Promise.reject("resolve");
+const promiseA = [promise1, promise2, promise3];
+Promise.all(promiseA).then((a) => log(a));
 
 
 log('Template Literals = > 백틱(`)으로 log 출력' );
